b89b228448dcbe50eb9c47069d157b0b1fd236b3
Raymundo Peralta <raymundo_peralta@yahoo.com>
2016-06-01 13:05:05 -0500

    Commit inicial

--- app/src/main/java/mx/peta/proyectob/MainActivity.java
+++ app/src/main/java/mx/peta/proyectob/MainActivity.java
@@ -2,12 +2,33 @@ package mx.peta.proyectob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button   btnSaludar;
+    private TextView lblMensaje;
+    private EditText etNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lblMensaje = (TextView)findViewById(R.id.lblMensaje);
+        etNombre   = (EditText)findViewById(R.id.tfNombre);
+
+
+        btnSaludar = (Button)findViewById(R.id.btnSaludar);
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lblMensaje.setText("Hola " + etNombre.getText());
+            }
+        });
+
+
     }
 }
